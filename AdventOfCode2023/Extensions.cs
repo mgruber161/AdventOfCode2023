a6105930873ee9dbf57f29ce3a93daa8e260cbdf
Manuel Gruber <grubermk97@gmail.com>
2023-12-08 14:27:03 +0100

    Solved Day08.

--- AdventOfCode2023/Extensions.cs
+++ AdventOfCode2023/Extensions.cs
@@ -1,14 +1,25 @@
-public static class Extensions
+namespace AdventOfCode2023
 {
-    public static long ToInt64(this string input)
+    public static class Extensions
     {
-        var num = 0L;
-        for (var i = 0; i < input.Length; i++)
+        public static long ToInt64(this string input)
         {
-            num = num * 10 + (input[i] - '0');
+            var num = 0L;
+            for (var i = 0; i < input.Length; i++)
+            {
+                num = num * 10 + (input[i] - '0');
+            }
+            return num;
         }
-        return num;
-    }
 
-    public static long[] ToInt64(this string[] values) => [.. values.Select(value => value.ToInt64())];
+        public static long[] ToInt64(this string[] values) => [.. values.Select(value => value.ToInt64())];
+
+        public static long LCM(this long[] numbers) => numbers.Aggregate((S, val) => S * val / GCD(S, val));
+
+        static long GCD(long n1, long n2)
+        {
+            if (n2 == 0) return n1;
+            else return GCD(n2, n1 % n2);
+        }
+    }
 }
---
+++ AdventOfCode2023/Problems/Day08.cs
@@ -0,0 +1,63 @@
+﻿using System.Text.RegularExpressions;
+using System.Threading.Channels;
+
+namespace AdventOfCode2023.Problems
+{
+    internal class Day08 : IProblem
+    {
+        public void Solve(string[] input)
+        {
+            var directionsDictionary = new Dictionary<string, Direction>();
+            var leftRightInstructions = input[0];
+
+            //parse
+            for (int i = 2; i < input.Length; i++)
+            {
+                var letters = new Regex("[A-Z]+").Matches(input[i]).Cast<Match>().Select(l => l.Value).ToArray();
+                directionsDictionary.Add(letters[0], new Direction(letters[1], letters[2]));
+            }
+
+            //Part 1
+            var steps = 0L;
+            var currentLocation = "AAA";
+            while (currentLocation != "ZZZ")
+            {
+                foreach (var instruction in leftRightInstructions)
+                {
+                    currentLocation = instruction == 'L' ? directionsDictionary[currentLocation].Left : directionsDictionary[currentLocation].Right;
+                    steps++;
+                    if (currentLocation == "ZZZ") break;
+                }
+            }
+
+            Console.WriteLine($"Part 1: {steps}");
+
+            //Part 2
+            var currentLocations = directionsDictionary.Keys.Where(x => x.EndsWith('A')).ToList();
+            var zDict = new Dictionary<int, List<long>> { { 0, new()}, { 1, new() }, { 2, new() }, { 3, new() }, { 4, new() }, { 5, new() } };
+
+            for (int i = 0; i < currentLocations.Count; i++)
+            {
+                steps = 0L;
+                var zCount = 0;
+                while (zCount < 2)
+                {
+                    foreach (var instruction in leftRightInstructions)
+                    {
+                        currentLocations[i] = instruction == 'L' ? directionsDictionary[currentLocations[i]].Left : directionsDictionary[currentLocations[i]].Right;
+                        steps++;
+                        if (currentLocations[i].EndsWith('Z'))
+                        {
+                            zDict[i].Add(steps);
+                            zCount++;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine($"Part 2: {zDict.Values.Select(x => x[1] - x[0]).ToArray().LCM()}");
+        }
+
+        public record Direction(string Left, string Right);
+    }
+}
