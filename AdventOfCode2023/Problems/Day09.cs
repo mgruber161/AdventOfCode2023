4995a5271ded58717f3afffb3982b037f089a986
Manuel Gruber <grubermk97@gmail.com>
2023-12-09 10:08:50 +0100

    Solved Day09.

---
+++ AdventOfCode2023/Problems/Day09.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2023.Problems
+{
+    internal class Day09 : IProblem
+    {
+        public void Solve(string[] input)
+        {
+            //Part 1
+            var sum = 0L;
+            foreach (var line in input)
+            {
+                var originalLine = new Regex(@"([-]?\d+)").Matches(line).Cast<Match>().Select(m => long.Parse(m.Value)).ToList();
+                sum += PredictNextValue(originalLine);
+            }
+            Console.WriteLine($"Part 1: {sum}");
+
+            //Part 2
+            sum = 0L;
+            foreach (var line in input)
+            {
+                var originalLine = new Regex(@"([-]?\d+)").Matches(line).Cast<Match>().Select(m => long.Parse(m.Value)).Reverse().ToList();
+                sum += PredictNextValue(originalLine);
+            }
+            Console.WriteLine($"Part 2: {sum}");
+        }
+
+        private long PredictNextValue(List<long> values)
+        {
+            if (values.TrueForAll(x => x == 0L)) return 0L;
+            return PredictNextValue(values.Zip(values.Skip(1), (a, b) => b - a).ToList()) + values[values.Count-1];
+        }
+    }
+}
